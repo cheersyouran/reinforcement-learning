b0ed59bc24e8ae1387af8c8de2e23223a03e46b4
“Youran” <cheersyouran@yeah.net>
2018-06-24 01:05:32 +0800

    a2c

---
+++ policygradient/advanced_actor_critic.py
@@ -0,0 +1,72 @@
+from keras.models import Model
+from keras.layers import Input, Dense, Add
+from keras.optimizers import sgd
+import numpy as np
+import pandas as pd
+from keras import backend as K
+np.random.seed(0)
+
+class Actor:
+    def __init__(self, n_features, n_actions, lr):
+        self.n_features = n_features
+        self.n_actions = n_actions
+        self.lr = lr
+        self.model = None
+
+        def mycrossentropy(y_true, y_pred):
+            return - K.log(y_pred) * y_true
+
+        a = Input(shape=(self.n_features,))
+        b = Dense(20, activation='relu')(a)
+        d = Dense(self.n_actions, activation='softmax')(b)
+        self.model = Model(inputs=a, outputs=d)
+
+        self.model.compile(loss='categorical_crossentropy', optimizer=sgd(lr=self.lr), metrics=['mse', 'accuracy'])
+
+    def learn(self, s, q):
+        s = s[np.newaxis, :]
+        self.model.train_on_batch(s, q)
+
+    def choose_action(self, s):
+        s = s[np.newaxis, :]
+        actions = self.model.predict(s)
+        choice = np.random.choice(self.n_actions, p=actions.flatten())
+        return choice
+
+class Critic:
+    def __init__(self, n_features, lr):
+        self.n_features = n_features
+        self.lr = lr
+
+        a = Input(shape=(self.n_features,))
+        b = Dense(20, activation='relu')(a)
+        d = Dense(1)(b)
+        self.model = Model(inputs=a, outputs=d)
+        self.model.compile(loss='mse', optimizer=sgd(lr=self.lr), metrics=['mse', 'accuracy'])
+
+    def learn(self, s, td):
+        s = s[np.newaxis, :]
+        self.model.train_on_batch(s, td)
+
+    def eval(self, s):
+        s = s[np.newaxis, :]
+        return self.model.predict(s)
+
+
+class Memory:
+    def __init__(self):
+        self.memory = pd.DataFrame(columns=['s', 'a', 'r', 's_'])
+
+    def store_transition(self, s, a, r, s_):
+        self.memory = self.memory.append({'s': s, 'a': a, 'r': r, 's_': s_}, ignore_index=True)
+
+    def sample(self, n):
+        indices = np.random.choice(self.memory.shape[0], n)
+        samples = self.memory.iloc[indices, :]
+
+        s = np.vstack(samples['s'])
+        r = np.vstack(samples['r'])
+        a = np.vstack(samples['a'])
+        s_ = np.vstack(samples['s_'])
+
+        return s, r, a, s_
---
+++ policygradient/cart_pole.py
@@ -0,0 +1,36 @@
+import gym
+import numpy as np
+from keras.utils import to_categorical
+
+from policygradient.advanced_actor_critic import Actor, Critic, Memory
+
+env = gym.make('CartPole-v0')
+# env = env.unwrapped
+np.random.seed(0)
+
+n_freatures = env.observation_space.shape[0]
+n_actions = env.action_space.n
+
+actor = Actor(n_features=n_freatures, lr=0.01, n_actions=n_actions)
+critic = Critic(n_features=n_freatures, lr=0.01)
+memory = Memory()
+
+for e in range(10000):
+    obs = env.reset()
+    total_reward = 0
+    while True:
+        env.render()
+        a = actor.choose_action(obs)
+        obs_, r, done, info = env.step(a)
+        if done:
+            r = 0
+        total_reward += r
+        td = r + 0.9 * critic.eval(obs_)
+        actor.learn(obs,  to_categorical(a, num_classes=2) * (td - critic.eval(obs)))
+        critic.learn(obs, td)
+        obs = obs_
+        if done:
+            print('[episode]:', e, ' [rewards]: ', total_reward)
+            break
+
+print('Finish!')
